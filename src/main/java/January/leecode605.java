ad7d35bd225f74453ecdf30b0feeba72a42d9d99
liqian <1029988609@qq.com>
2021-01-01 20:56:41 +0800

    2021.1.1:第605题

---
+++ src/main/java/January/leecode605.java
@@ -0,0 +1,41 @@
+package January;
+
+import org.junit.Test;
+
+public class leecode605 {
+    // 种花问题 贪心算法解决
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int res = 0;
+        for(int i=0;i<flowerbed.length;i++){
+            // 1.首位置
+            if(i==0){
+                if((i+1<flowerbed.length) && (flowerbed[i]+flowerbed[i+1])==0){
+                    flowerbed[i] = 1;
+                    res++;
+                }else if(flowerbed.length ==1 && flowerbed[i]==0) {
+                    flowerbed[i] = 1;
+                    res++;
+                }
+            }else if(i==flowerbed.length-1){
+                // 2.末位置
+                if((flowerbed[i]+flowerbed[i-1])==0){
+                    flowerbed[i] = 1;
+                    res++;
+                }
+            }else {
+                // 3.中间位置
+                if((flowerbed[i-1]+flowerbed[i]+flowerbed[i+1])==0){
+                    flowerbed[i] = 1;
+                    res++;
+                }
+            }
+        }
+        return res>=n;
+    }
+
+    @Test
+    public void test(){
+        int[] arr = {1,0,0,0,1};
+        System.out.println(canPlaceFlowers(arr,1));
+    }
+}
