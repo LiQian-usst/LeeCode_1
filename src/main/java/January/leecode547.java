46d4bbb1841404989ffaa379267665b9f9b1d1df
liqian <1029988609@qq.com>
2021-01-07 22:47:17 +0800

    2021.1.7:第547题

---
+++ src/main/java/January/leecode547.java
@@ -0,0 +1,27 @@
+package January;
+
+public class leecode547 {
+    // 省份数量 nxn矩阵
+    public int findCircleNum(int[][] isConnected) {
+        int provinces = isConnected.length;
+        boolean[] visited = new boolean[provinces];
+        int circles = 0;
+        for (int i = 0; i < provinces; i++) {
+            if (!visited[i]) {
+                dfs(isConnected, visited, provinces, i);
+                circles++;
+            }
+        }
+        return circles;
+    }
+
+    public void dfs(int[][] isConnected, boolean[] visited, int provinces, int i) {
+        for (int j = 0; j < provinces; j++) {
+            if (isConnected[i][j] == 1 && !visited[j]) {
+                visited[j] = true;
+                dfs(isConnected, visited, provinces, j);
+            }
+        }
+    }
+
+}
