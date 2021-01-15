0072f32c78abc6f9c8f26004fd325bf24c08f930
liqian <1029988609@qq.com>
2021-01-15 23:14:51 +0800

    2021.1.15:第467题

---
+++ src/main/java/January/leecode467.java
@@ -0,0 +1,31 @@
+package January;
+
+public class leecode467 {
+    // 环绕字符串中唯一的字符串
+    public int findSubstringInWraproundString(String p) {
+        // 记录 p 中以每个字符结尾的最长连续子串的长度
+        int[] dp = new int[26];
+        char[] array = p.toCharArray();
+        // 记录当前连续子串的长度
+        int count = 0;
+        // 遍历 p 中的所有字符
+        for (int i = 0; i < array.length; i++) {
+            // 判断字符是否连续
+            if (i > 0 && (array[i] - array[i - 1] - 1) % 26 == 0) {
+                // 连续则自加
+                count++;
+            } else {
+                // 不连续则刷新
+                count = 1;
+            }
+            // 只存储最长的连续长度
+            dp[array[i] - 'a'] = Math.max(dp[array[i] - 'a'], count);
+        }
+        int result = 0;
+        // 统计所有以每个字符结尾的最长连续子串的长度，就是唯一相等子串的个数
+        for (int i : dp) {
+            result += i;
+        }
+        return result;
+    }
+}
